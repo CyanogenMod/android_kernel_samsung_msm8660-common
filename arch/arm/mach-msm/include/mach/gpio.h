41c797850b0047bcbd5bb7b2fae883f38e13757a
Michael Bohan <mbohan@codeaurora.org>
2012-01-17 09:51:50 -0800

    msm: gpio-v2: Add support for irq_domains
    irq_domains are necessary to support interrupt chip drivers with
    the Device Tree. In addition, by registering with irq_domains,
    other irq chip drivers on the system will respect the virq
    allocations made by this driver.
    
    The msm-gpio specification requires two interrupt-cells. The
    first specifies the irq chip hwirq, and the second is a default
    irq type configuration for the interrupt. It's still possible to
    override the default configuration with request_irq() if one
    chooses to.
    
    Change-Id: If2d3ce321f4c9ec83b0ee41244533696192e1d42
    Signed-off-by: Michael Bohan <mbohan@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/include/mach/gpio.h

--- arch/arm/mach-msm/include/mach/gpio.h
+++ arch/arm/mach-msm/include/mach/gpio.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
  * Author: Mike Lockwood <lockwood@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -254,4 +254,10 @@ static inline int msm_gpio_install_direct_irq(unsigned gpio, unsigned irq,
 void msm_tlmm_set_spkr_hdrive(enum msm_tlmm_spkr_hdrive_tgt tgt, int drv_str);
 void msm_tlmm_set_spkr_pull(enum msm_tlmm_spkr_pull_tgt tgt, int drv_str);
 #endif
+
+#ifdef CONFIG_OF
+int __init msm_gpio_of_init(struct device_node *node,
+			    struct device_node *parent);
+#endif
+
 #endif /* __ASM_ARCH_MSM_GPIO_H */
