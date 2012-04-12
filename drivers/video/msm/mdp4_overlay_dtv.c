9a1aa9aa2d473498a4f523237cf5f643896a338e
Huaibin Yang <huaibiny@codeaurora.org>
2012-04-11 23:37:15 -0700

    msm_fb: display: add dtv_pipe null check before dereferenc it
    There are chances that a null dtv_pipe pointer is dereferenced and
    system crashes. One such case is unplug HDMI cable while playing
    video. So a null check is needed for each dtv_pipe dereference.
    
    CRs-fixed: 355238
    Change-Id: I1ff57b00169e217fc5ed98574db7fd49b9319d0a
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -459,6 +459,12 @@ static void mdp4_overlay_dtv_ov_start(struct msm_fb_data_type *mfd)
 	if (mfd->ov_start)
 		return;
 
+	if (!dtv_pipe) {
+		pr_debug("%s: no mixer1 base layer pipe allocated!\n",
+			 __func__);
+		return;
+	}
+
 	if (dtv_pipe->blt_addr) {
 		mdp4_dtv_blt_ov_update(dtv_pipe);
 		dtv_pipe->ov_cnt++;
@@ -509,6 +515,12 @@ static void mdp4_overlay_dtv_wait4_ov_done(struct msm_fb_data_type *mfd,
 		return;
 	if (!(data & 0x1) || (pipe == NULL))
 		return;
+	if (!dtv_pipe) {
+		pr_debug("%s: no mixer1 base layer pipe allocated!\n",
+			 __func__);
+		return;
+	}
+
 	wait_for_completion_timeout(&dtv_pipe->comp,
 			msecs_to_jiffies(VSYNC_PERIOD*2));
 	mdp_disable_irq(MDP_OVERLAY1_TERM);
@@ -552,11 +564,15 @@ void mdp4_overlay_dtv_wait_for_ov(struct msm_fb_data_type *mfd,
 
 void mdp4_dma_e_done_dtv()
 {
+	if (!dtv_pipe)
+		return;
+
 	complete(&dtv_pipe->comp);
 }
 
 void mdp4_external_vsync_dtv()
 {
+
 	complete_all(&dtv_comp);
 }
 
@@ -565,6 +581,8 @@ void mdp4_external_vsync_dtv()
  */
 void mdp4_overlay1_done_dtv()
 {
+	if (!dtv_pipe)
+		return;
 	if (dtv_pipe->blt_addr) {
 		mdp4_dtv_blt_dmae_update(dtv_pipe);
 		dtv_pipe->dmae_cnt++;
@@ -631,6 +649,12 @@ static void mdp4_dtv_do_blt(struct msm_fb_data_type *mfd, int enable)
 		return;
 	}
 
+	if (!dtv_pipe) {
+		pr_debug("%s: no mixer1 base layer pipe allocated!\n",
+			 __func__);
+		return;
+	}
+
 	spin_lock_irqsave(&mdp_spin_lock, flag);
 	if (enable && dtv_pipe->blt_addr == 0) {
 		dtv_pipe->blt_addr = mfd->ov1_wb_buf->phys_addr;
@@ -671,7 +695,11 @@ void mdp4_dtv_overlay(struct msm_fb_data_type *mfd)
 	struct mdp4_overlay_pipe *pipe;
 	if (!mfd->panel_power_on)
 		return;
-
+	if (!dtv_pipe) {
+		pr_debug("%s: no mixer1 base layer pipe allocated!\n",
+			 __func__);
+		return;
+	}
 	mutex_lock(&mfd->dma->ov_mutex);
 	if (dtv_pipe == NULL) {
 		if (mdp4_overlay_dtv_set(mfd, NULL)) {
