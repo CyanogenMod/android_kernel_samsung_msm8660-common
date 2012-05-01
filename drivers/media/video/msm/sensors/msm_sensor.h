630fe0a53c59e24c367f87570364b683101e5a11
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2012-05-01 09:38:08 -0700

    msm: camera: Adjust exposure setting
    While changing between preview and snapshot modes, different
    frame length line is configured. Coarse integration time may
    become invalid for some frame length line values. Validate and
    reconfigure coarse integration time while switching between
    modes
    
    CRs-fixed: 356564 358194
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
    (cherry picked from commit 6a6ccd619fb9a8317e4f6673b9af76abe213633a)
    
    Conflicts:
    
    	drivers/media/video/msm/sensors/msm_sensor.h
    
    Change-Id: I929f1f4c6bf57de36918afabf9816e2bb4e3ff42
    Signed-off-by: Ramakrishna Prasad N <crpn@codeaurora.org>

--- drivers/media/video/msm/sensors/msm_sensor.h
+++ drivers/media/video/msm/sensors/msm_sensor.h
@@ -130,6 +130,8 @@ struct msm_sensor_fn_t {
 	int (*sensor_power_down)
 		(struct msm_sensor_ctrl_t *);
 	int (*sensor_power_up) (struct msm_sensor_ctrl_t *);
+	int (*sensor_adjust_frame_lines)
+		(struct msm_sensor_ctrl_t *s_ctrl, uint16_t res);
 };
 
 struct msm_sensor_ctrl_t {
@@ -218,6 +220,9 @@ int msm_sensor_write_res_settings
 	(struct msm_sensor_ctrl_t *s_ctrl, uint16_t res);
 
 int32_t msm_sensor_write_output_settings(struct msm_sensor_ctrl_t *s_ctrl,
+	uint16_t res);
+
+int32_t msm_sensor_adjust_frame_lines(struct msm_sensor_ctrl_t *s_ctrl,
 	uint16_t res);
 
 int32_t msm_sensor_setting(struct msm_sensor_ctrl_t *s_ctrl,
