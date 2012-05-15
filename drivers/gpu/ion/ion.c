3f8c0175ece036c451233a29605814a437e374a6
Olav Haugan <ohaugan@codeaurora.org>
2012-05-15 14:38:09 -0700

    gpu: ion: Ensure ioctl return correct error codes
    Under certain error conditions the following ION ioctl's would return
    success even when the operation failed:
    
    -ION_IOC_CLEAN_CACHES
    -ION_IOC_INV_CACHES
    -ION_IOC_CLEAN_INV_CACHES
    
    Add return of correct error codes when an error occurs.
    
    Change-Id: Ie8824bfecbb423c23d32c6a3d7058f06d8322906
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1564,6 +1564,8 @@ static long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		if (!data.handle)
 			ion_free(client, handle);
 
+		if (ret < 0)
+			return ret;
 		break;
 
 	}
