a3e57fc668ffcfd8d1db1059e0c1f82710bad494
Vidyakumar Athota <vathota@codeaurora.org>
2012-05-16 14:31:39 +0530

    msm: Asoc: audio: Migrate to ION from pmem, dma alloc
    -Voice driver and RT proxy driver are using Pmem and
    DMA alloc. Migrate them to use ION memory to save memory
    and avoid stability issues.
    
    CRs-Fixed: 369303
    Change-Id: If58b69b9f8b7b03661a36451c1a5c27885fbf976
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

--- sound/soc/msm/qdsp6/q6voice.h
+++ sound/soc/msm/qdsp6/q6voice.h
@@ -13,6 +13,7 @@
 #define __QDSP6VOICE_H__
 
 #include <mach/qdsp6v2/apr.h>
+#include <linux/ion.h>
 
 #define MAX_VOC_PKT_SIZE 642
 #define SESSION_NAME_LEN 20
@@ -872,6 +873,7 @@ struct voice_data {
 };
 
 struct cal_mem {
+	struct ion_handle *handle;
 	uint32_t phy;
 	void *buf;
 };
@@ -892,6 +894,7 @@ struct common_data {
 	/* APR to CVP in the Q6 */
 	void *apr_q6_cvp;
 
+	struct ion_client *client;
 	struct cal_mem cvp_cal;
 	struct cal_mem cvs_cal;
 
