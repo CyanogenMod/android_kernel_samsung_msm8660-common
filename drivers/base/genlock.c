205327209553cd9d4cf62db34d1e3b6e5366bf41
Jeff Boody <jboody@codeaurora.org>
2012-06-07 11:02:22 -0600

    base: genlock: protect kref counting with spinlock
    A race condition exists where the attach may not kref_get
    before the kref_put finished. The genlock_file_lock was
    originally added to protect against this case however it
    was not protecting the kref.
    
    CRs-fixed: 359649
    Signed-off-by: Jeff Boody <jboody@codeaurora.org>
    (cherry picked from commit d6f534fc51eb5fc23d08dacceecd42a4b98747a6)
    
    Change-Id: I4115ea1cc39ebe38ad0032ebf86871ac9d23a312
    Signed-off-by: Ramakrishna Prasad N <crpn@codeaurora.org>
    (cherry picked from commit 295371180c3c46c0078ad247a53e1c8364aeee1b)
    
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>
    
    Conflicts:
    
    	drivers/base/genlock.c

--- drivers/base/genlock.c
+++ drivers/base/genlock.c
@@ -65,7 +65,7 @@ static struct kmem_cache *genlock_lock_cachep;
  * released while another process tries to attach it
  */
 
-static DEFINE_SPINLOCK(genlock_file_lock);
+static DEFINE_SPINLOCK(genlock_ref_lock);
 
 static void genlock_destroy(struct kref *kref)
 {
@@ -88,6 +88,8 @@ static void genlock_destroy(struct kref *kref)
 		else
 			kfree(lock);
 	}
+	if (lock->file)
+		lock->file->private_data = NULL;
 
 }
 
@@ -225,12 +227,13 @@ struct genlock *genlock_attach_lock(struct genlock_handle *handle, int fd)
 	 * released and then attached
 	 */
 
-	spin_lock(&genlock_file_lock);
+	spin_lock(&genlock_ref_lock);
 	lock = file->private_data;
 
 	fput(file);
 
 	if (lock == NULL) {
+		spin_unlock(&genlock_ref_lock);
 		GENLOCK_LOG_ERR("File descriptor is invalid\n");
 		spin_unlock(&genlock_file_lock);
 		return ERR_PTR(-EINVAL);
@@ -239,7 +242,7 @@ struct genlock *genlock_attach_lock(struct genlock_handle *handle, int fd)
 	handle->lock = lock;
 	
 	kref_get(&lock->refcount);
-	spin_unlock(&genlock_file_lock);
+	spin_unlock(&genlock_ref_lock);
 
 	return lock;
 }
@@ -666,12 +669,12 @@ static void genlock_release_lock(struct genlock_handle *handle)
 	
 	spin_unlock_irqrestore(&handle->lock->lock, flags);
 
-	spin_lock(&genlock_file_lock);
-	atomic_dec(&handle->lock->active_users);
+	spin_lock(&genlock_ref_lock);
+        atomic_dec(&handle->lock->active_users);
 	kref_put(&handle->lock->refcount, genlock_destroy);
-	handle->lock = NULL; 
+	spin_unlock(&genlock_ref_lock);
+	handle->lock = NULL;
 	handle->active = 0;
-	spin_unlock(&genlock_file_lock);
 }
 
 /*
