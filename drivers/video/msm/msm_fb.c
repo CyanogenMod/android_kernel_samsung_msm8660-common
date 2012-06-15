7f2c75ac293287cb0885db46db4a6b56bbcd8382
Laura Abbott <lauraa@codeaurora.org>
2012-06-15 07:43:20 -0700

    video: msm: Don't make the framebuffer cached
    The video framebuffer should not be cached when mapping into the
    rotator or video domains. Change it.
    
    Change-Id: I407c68ac21a030aff801ebfae01d360bb6bf338c
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1511,7 +1511,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 					GEN_POOL,
 					fbi->fix.smem_len,
 					SZ_4K,
-					1,
+					0,
 					&(mfd->display_iova));
 
 	msm_iommu_map_contig_buffer(fbi->fix.smem_start,
@@ -1519,7 +1519,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 					GEN_POOL,
 					fbi->fix.smem_len,
 					SZ_4K,
-					1,
+					0,
 					&(mfd->rotator_iova));
 
 	if (!bf_supported || mfd->index == 0)
