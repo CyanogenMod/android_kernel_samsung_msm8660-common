ab2a075c4fe93b75c4ff931e34713584fafe44f5
Jeff Ohlstein <johlstei@codeaurora.org>
2012-07-17 16:45:19 -0700

    tick-sched: recalculate sleep length each time it is requested
    Recalculating the sleep length each time its called allows us to account
    for the fact that the amount of time we can sleep for might change after
    tick_nohz_stop_sched_tick is called in idle. The prime example of this
    is an idle notifier that cancels timers as we are entering idle.
    
    Change-Id: I92871efc7befb3fee2a816da16145ba9da334a9e
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>
    
    Conflicts:
    
    	kernel/time/tick-sched.c

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -453,7 +453,6 @@ void tick_nohz_stop_sched_tick(int inidle)
 out:
 	ts->next_jiffies = next_jiffies;
 	ts->last_jiffies = last_jiffies;
-	ts->sleep_length = ktime_sub(dev->next_event, now);
 end:
 	local_irq_restore(flags);
 }
@@ -466,8 +465,8 @@ end:
 ktime_t tick_nohz_get_sleep_length(void)
 {
 	struct tick_sched *ts = &__get_cpu_var(tick_cpu_sched);
-
-	return ts->sleep_length;
+	struct clock_event_device *dev = __get_cpu_var(tick_cpu_device).evtdev;
+	return ktime_sub(dev->next_event, ts->idle_entrytime);
 }
 
 static void tick_nohz_restart(struct tick_sched *ts, ktime_t now)
