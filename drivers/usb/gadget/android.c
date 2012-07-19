f46705a9396ffa59fde663cbfdac6b983d41b599
Steve Kondik <shade@chemlab.org>
2012-07-18 17:59:06 -0700

    Revert "usb: gadget: android: Fix product name"
    This reverts commit 89e8b7ba0223f98a1afbf3a8fe037dd5b725a736.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1649,7 +1649,10 @@ field ## _store(struct device *dev, struct device_attribute *attr,	\
 		const char *buf, size_t size)				\
 {									\
 	if (size >= sizeof(buffer)) return -EINVAL;			\
-	return strlcpy(buffer, buf, sizeof(buffer));			\
+	if (sscanf(buf, "%255s", buffer) == 1) {			\
+		return size;						\
+	}								\
+	return -1;							\
 }									\
 static DEVICE_ATTR(field, S_IRUGO | S_IWUSR, field ## _show, field ## _store);
 
