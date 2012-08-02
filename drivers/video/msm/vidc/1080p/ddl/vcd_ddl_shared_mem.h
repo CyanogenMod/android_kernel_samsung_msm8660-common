8e058e44ce87a7a5a9c10cc27d78ac439e8c23cd
Deepak Verma <dverma@codeaurora.org>
2012-08-02 16:25:39 +0530

    msm: vidc: Add PAR value extradata support for H264 and Mpeg2
    Core gives aspect ratio information for H264/Mpeg2 in a
    shared memory register. This change will fetch the value
    of the aspect ratio from the designated register and append
    this information to the frame_info extradata which can be
    used by the client.
    
    Change-Id: Iacacaf1b1181546ba2d09362a51bbe5178a63999
    Signed-off-by: Deepak Verma <dverma@codeaurora.org>

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_shared_mem.h
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_shared_mem.h
@@ -183,7 +183,7 @@ void vidc_sm_set_decoder_stuff_bytes_consumption(
 void vidc_sm_set_video_core_timeout_value(struct ddl_buf_addr *shared_mem,
     u32 timeout);
 void vidc_sm_get_aspect_ratio_info(struct ddl_buf_addr *shared_mem,
-	struct vcd_aspect_ratio *aspect_ratio_info);
+	enum vcd_codec codec, struct vcd_aspect_ratio *aspect_ratio_info);
 void vidc_sm_set_encoder_slice_batch_int_ctrl(struct ddl_buf_addr *shared_mem,
     u32 slice_batch_int_enable);
 void vidc_sm_get_num_slices_comp(struct ddl_buf_addr *shared_mem,
