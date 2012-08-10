5a4ccf2962ed1d8c2ff63080fd949c0a7211c1e5
mikeyman77 <mikeyman77@gmail.com>
2012-08-09 21:45:55 -0700

    Revert "mmc: sdio: avoid spurious calls to interrupt handlers"
    This reverts commit 790a8faedc4f079dc6ff3099f3133570a95b41e5.

--- drivers/mmc/core/sdio_irq.c
+++ drivers/mmc/core/sdio_irq.c
@@ -27,23 +27,10 @@
 
 #include "sdio_ops.h"
 
-static int process_sdio_pending_irqs(struct mmc_host *host)
+static int process_sdio_pending_irqs(struct mmc_card *card)
 {
-	struct mmc_card *card = host->card;
 	int i, ret, count;
 	unsigned char pending;
-	struct sdio_func *func;
-
-	/*
-	 * Optimization, if there is only 1 function interrupt registered
-	 * and we know an IRQ was signaled then call irq handler directly.
-	 * Otherwise do the full probe.
-	 */
-	func = card->sdio_single_irq;
-	if (func && host->sdio_irq_pending) {
-		func->irq_handler(func);
-		return 1;
-	}
 
 	ret = mmc_io_rw_direct(card, 0, 0, SDIO_CCCR_INTx, 0, &pending);
 	if (ret) {
@@ -117,8 +104,7 @@ static int sdio_irq_thread(void *_host)
 		ret = __mmc_claim_host(host, &host->sdio_irq_thread_abort);
 		if (ret)
 			break;
-		ret = process_sdio_pending_irqs(host);
-		host->sdio_irq_pending = false;
+		ret = process_sdio_pending_irqs(host->card);
 		mmc_release_host(host);
 
 		/*
