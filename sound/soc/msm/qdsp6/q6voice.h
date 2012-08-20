f84e0cdffc206c7a068d2317444fd1cae4271726
Vidyakumar Athota <vathota@codeaurora.org>
2012-08-20 12:11:06 -0700

    Revert "msm: Asoc: audio: Migrate to ION from pmem, dma alloc"
    This reverts commit a3e57fc668ffcfd8d1db1059e0c1f82710bad494.
    
    Change-Id: I75fbb4fa4d3b2371174af8d5ed44d54bdde06827
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

--- sound/soc/msm/qdsp6/q6voice.h
+++ sound/soc/msm/qdsp6/q6voice.h
@@ -13,7 +13,6 @@
 #define __QDSP6VOICE_H__
 
 #include <mach/qdsp6v2/apr.h>
-#include <linux/ion.h>
 
 #define MAX_VOC_PKT_SIZE 642
 #define SESSION_NAME_LEN 20
@@ -873,7 +872,6 @@ struct voice_data {
 };
 
 struct cal_mem {
-	struct ion_handle *handle;
 	uint32_t phy;
 	void *buf;
 };
@@ -894,7 +892,6 @@ struct common_data {
 	/* APR to CVP in the Q6 */
 	void *apr_q6_cvp;
 
-	struct ion_client *client;
 	struct cal_mem cvp_cal;
 	struct cal_mem cvs_cal;
 
