842dd74f4fcef0ca0b7aabf131feae4dea6974c1
mikeyman77 <mikeyman77@gmail.com>
2012-08-28 16:04:44 -0700

    vibetonez: disable debug logging
    Change-Id: Ib360ffab35424b5e6c616b6ee6bc668cf66f0989

--- drivers/misc/vibetonz/tspdrv.c
+++ drivers/misc/vibetonz/tspdrv.c
@@ -135,7 +135,7 @@ static DECLARE_WORK(vibetonz_work, _set_vibetonz_work);
 static int set_vibetonz(int timeout)
 {
 	if(!timeout) {
-		printk("[VIBETONZ] DISABLE\n");
+		//printk("[VIBETONZ] DISABLE\n");
 #if defined (CONFIG_KOR_MODEL_SHV_E110S)		
 		if (get_hw_rev() > 0x00 ){
 			vib_isa1200_onoff(0);	
@@ -170,7 +170,7 @@ static int set_vibetonz(int timeout)
 		gpio_set_value(VIB_EN, VIBRATION_OFF);	
 #endif		
 	} else {
-		printk("[VIBETONZ] ENABLE\n");
+		//printk("[VIBETONZ] ENABLE\n");
 #if defined (CONFIG_KOR_MODEL_SHV_E110S)		
 		if (get_hw_rev() > 0x00 ){
 			vibtonz_en(1);
@@ -253,7 +253,7 @@ static int get_time_for_vibetonz(struct timed_output_dev *dev)
 
 static void enable_vibetonz_from_user(struct timed_output_dev *dev,int value)
 {
-	printk("[VIBETONZ] %s : time = %d msec \n",__func__,value);
+	//printk("[VIBETONZ] %s : time = %d msec \n",__func__,value);
 	hrtimer_cancel(&timer);
 	
 	/* set_vibetonz(value); */
