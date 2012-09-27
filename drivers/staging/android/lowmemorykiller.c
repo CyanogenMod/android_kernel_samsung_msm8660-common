5261606a64449d0095d04739ae3225ba79b587d5
Arve Hjønnevåg <arve@android.com>
2012-09-26 19:52:59 -0700

    staging: android: lowmemorykiller: Change default debug_level to 1
    The select...to kill messages are not very useful when not debugging
    the lowmemorykiller itself. After the change to check TIF_MEMDIE
    instead of using a task notifer this message can also get very
    noisy.
    
    Change-Id: Ice171c25801d6faa454b885a23b24b002423b754
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -41,7 +41,7 @@
 #include <linux/memory.h>
 #include <linux/memory_hotplug.h>
 
-static uint32_t lowmem_debug_level = 2;
+static uint32_t lowmem_debug_level = 1;
 static int lowmem_adj[6] = {
 	0,
 	1,
