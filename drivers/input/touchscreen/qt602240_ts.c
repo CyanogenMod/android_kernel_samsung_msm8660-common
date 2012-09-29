5bdaf717e7ae86e4a103757e32e24fa1f89da650
David Ferguson <ferguson.david@gmail.com>
2012-09-29 18:12:57 -0400

    qt602240_ts: add additional mutex protection
    Change-Id: I174920431e0eaba2b59dda78e478af228affb9d7

--- drivers/input/touchscreen/qt602240_ts.c
+++ drivers/input/touchscreen/qt602240_ts.c
@@ -199,6 +199,8 @@ struct mxt224_data {
 	struct timer_list autocal_timer;
 };
 
+static struct mutex lock;
+static bool lock_needs_init = 1;
 struct mxt224_data *copy_data;
 extern struct class *sec_class;
 int touch_is_pressed;
@@ -249,7 +251,7 @@ static unsigned int gResume_flag = 0;
 static unsigned char not_yet_count = 0;
 
 #endif
-static void mxt224_optical_gain(u8 family_id, uint16_t dbg_mode);
+//static void mxt224_optical_gain(u8 family_id, uint16_t dbg_mode);
 static void TSP_forced_release_for_call(void);
 static int tsp_pattern_tracking(int fingerindex, s16 x, s16 y);
 static void report_input_data(struct mxt224_data *data);
@@ -579,6 +581,10 @@ static void mxt224_ta_probe(int ta_status)
 		return;
 	}
 
+	if (lock_needs_init) {
+		mutex_init(&lock);
+		lock_needs_init = 0;
+	}
 
 	if (ta_status) {
 		threshold = 70;
@@ -809,7 +815,7 @@ void check_chip_calibration(unsigned char one_touch_input_flag)
 	
 	#if defined(CONFIG_USA_MODEL_SGH_I577) || defined(CONFIG_CAN_MODEL_SGH_I577R) || defined (CONFIG_USA_MODEL_SGH_I727) || defined (CONFIG_USA_MODEL_SGH_T989)
 	bool ta_status_check;
-	u8 val;
+	//u8 val;
 	#endif
 
 
@@ -1860,19 +1866,21 @@ static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
 	int id;
 	u8 msg[data->msg_object_size];
 	u8 touch_message_flag = 0;
-	u16 size_one;
-	u8 value, ret;
-	u16 obj_address = 0;
 	
 	#if defined (CONFIG_USA_MODEL_SGH_I577) || defined(CONFIG_CAN_MODEL_SGH_I577R)	|| defined (CONFIG_USA_MODEL_SGH_I727) || defined (CONFIG_USA_MODEL_SGH_T989)		
-	unsigned int register_address = 0;
 	#else
-	#ifndef CLEAR_MEDIAN_FILTER_ERROR
+	#ifdef CLEAR_MEDIAN_FILTER_ERROR
+	u16 size_one;
+	u8 value, ret;
+	bool ta_status=0;
+	u16 obj_address = 0;
+	#else
 	unsigned int register_address = 0;
 	#endif
 	#endif
-	bool ta_status=0;
 	
+	mutex_lock(&lock);
+
 	disable_irq_nosync(irq);
 
 #if defined (CONFIG_USA_MODEL_SGH_I577) || defined(CONFIG_CAN_MODEL_SGH_I577R) || defined (CONFIG_USA_MODEL_SGH_I727) || defined (CONFIG_USA_MODEL_SGH_T989)		
@@ -1892,7 +1900,7 @@ static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
 		
 		if (read_mem(data, data->msg_proc, sizeof(msg), msg)) {
 			enable_irq(irq);
-			return IRQ_HANDLED;
+			goto unlock;
 		}
 
 		/*
@@ -2165,6 +2173,8 @@ static irqreturn_t mxt224_irq_thread(int irq, void *ptr)
 
 	enable_irq(irq);
 
+unlock:
+	mutex_unlock(&lock);
 	return IRQ_HANDLED;
 }
 
@@ -2308,6 +2318,8 @@ static void mxt224_early_suspend(struct early_suspend *h)
 {
 	struct mxt224_data *data = container_of(h, struct mxt224_data,
 								early_suspend);
+	mutex_lock(&lock);
+
 	mxt224_enabled = 0;
 	touch_is_pressed = 0;
 	qt_timer_state = 0;	
@@ -2328,6 +2340,8 @@ static void mxt224_early_suspend(struct early_suspend *h)
 	free_irq(data->client->irq,mxt224_irq_thread);
 #endif
 	mxt224_internal_suspend(data);
+
+	mutex_unlock(&lock);
 	printk(KERN_ERR "[TSP] mxt224_early_suspend \n");
 }
 
@@ -2336,6 +2350,7 @@ static void mxt224_late_resume(struct early_suspend *h)
 	bool ta_status=0;
 	struct mxt224_data *data = container_of(h, struct mxt224_data,
 								early_suspend);
+	mutex_lock(&lock);
 	mxt224_internal_resume(data);
 #if 0 // #if defined (CONFIG_KOR_MODEL_SHV_E110S)
 	request_threaded_irq(data->client->irq, NULL, mxt224_irq_thread,
@@ -2363,6 +2378,7 @@ static void mxt224_late_resume(struct early_suspend *h)
 		mxt224_ta_probe(ta_status);
 	}
 	calibrate_chip();
+	mutex_unlock(&lock);
 	printk(KERN_ERR "[TSP] mxt224_late_resume \n");
 }
 #else
@@ -2555,8 +2571,10 @@ static ssize_t qt602240_object_setting(struct device *dev,
 
 	size = 1;
 	value = (u8)register_value;
+	mutex_lock(&lock);
 	write_mem(data, address+(u16)object_register, size, &value);
 	read_mem(data, address+(u16)object_register, (u8)size, &val);
+	mutex_unlock(&lock);
 
 	printk(KERN_ERR "[TSP] T%d Byte%d is %d\n", object_type, object_register, val);
 #if !defined (CONFIG_USA_MODEL_SGH_I577) && !defined(CONFIG_CAN_MODEL_SGH_I577R) && !defined (CONFIG_USA_MODEL_SGH_I727) && !defined (CONFIG_USA_MODEL_SGH_T989)
@@ -2585,10 +2603,12 @@ static ssize_t qt602240_object_show(struct device *dev,
 		printk(KERN_ERR "[TSP] fail to get object_info\n");
 		return count;
 	}
+	mutex_lock(&lock);
 	for (i = 0; i < size; i++) {
 		read_mem(data, address+i, 1, &val);
 		printk(KERN_ERR "[TSP] Byte %u --> %u\n", i, val);
 	}
+	mutex_unlock(&lock);
 	return count;
 }
 
@@ -2844,6 +2864,7 @@ int read_all_delta_data(uint16_t dbg_mode)
 }
 
 
+#if 0
 static void mxt224_optical_gain(u8 family_id, uint16_t dbg_mode)
 {
 	u8 read_page, read_point;
@@ -2919,6 +2940,7 @@ static void mxt224_optical_gain(u8 family_id, uint16_t dbg_mode)
 
 	printk(KERN_ERR "[TSP] -mxt224_optical_gain()\n");
 };
+#endif
 
 static int mxt224_check_bootloader(struct i2c_client *client,
 					unsigned int state)
@@ -3079,8 +3101,10 @@ static ssize_t set_tsp_module_off_show(struct device *dev, struct device_attribu
 	touch_is_pressed = 0;
 	Doing_calibration_falg = 0; 
 
+	mutex_lock(&lock);
 	disable_irq(copy_data->client->irq);
 	ret = mxt224_internal_suspend(copy_data);
+	mutex_unlock(&lock);
 
 	if (ret == 0)
 		*buf = '1';
@@ -3097,6 +3121,7 @@ static ssize_t set_tsp_module_on_show(struct device *dev, struct device_attribut
 	/*struct i2c_client *client = to_i2c_client(dev);
 	struct mxt224_data *data = i2c_get_clientdata(client);*/
 
+	mutex_lock(&lock);
 	copy_data->power_on();
 	msleep(70);
 
@@ -3111,6 +3136,7 @@ static ssize_t set_tsp_module_on_show(struct device *dev, struct device_attribut
 		mxt224_ta_probe(ta_status);
 	}
 	calibrate_chip();
+	mutex_unlock(&lock);
 
 	
 	if (ret == 0)
@@ -3124,42 +3150,54 @@ static ssize_t set_tsp_module_on_show(struct device *dev, struct device_attribut
 static ssize_t set_refer0_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_refrence = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_REFERENCE_MODE, test_node[0], &qt_refrence);
+	mutex_unlock(&lock);
 	return sprintf(buf, "%u\n", qt_refrence);
 }
 
 static ssize_t set_refer1_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_refrence = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_REFERENCE_MODE, test_node[1], &qt_refrence);
+	mutex_unlock(&lock);
 	return sprintf(buf, "%u\n", qt_refrence);
 }
 
 static ssize_t set_refer2_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_refrence = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_REFERENCE_MODE, test_node[2], &qt_refrence);
+	mutex_unlock(&lock);
 	return sprintf(buf, "%u\n", qt_refrence);
 }
 
 static ssize_t set_refer3_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_refrence = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_REFERENCE_MODE, test_node[3], &qt_refrence);
+	mutex_unlock(&lock);
 	return sprintf(buf, "%u\n", qt_refrence);
 }
 
 static ssize_t set_refer4_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_refrence = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_REFERENCE_MODE, test_node[4], &qt_refrence);
+	mutex_unlock(&lock);
 	return sprintf(buf, "%u\n", qt_refrence);
 }
 
 static ssize_t set_delta0_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_delta = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_DELTA_MODE, test_node[0], &qt_delta);
+	mutex_unlock(&lock);
 	if (qt_delta < 32767)
 		return sprintf(buf, "%u\n", qt_delta);
 	else
@@ -3171,7 +3209,9 @@ static ssize_t set_delta0_mode_show(struct device *dev, struct device_attribute 
 static ssize_t set_delta1_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_delta = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_DELTA_MODE, test_node[1], &qt_delta);
+	mutex_unlock(&lock);
 	if (qt_delta < 32767)
 		return sprintf(buf, "%u\n", qt_delta);
 	else
@@ -3183,7 +3223,9 @@ static ssize_t set_delta1_mode_show(struct device *dev, struct device_attribute 
 static ssize_t set_delta2_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_delta = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_DELTA_MODE, test_node[2], &qt_delta);
+	mutex_unlock(&lock);
 	if (qt_delta < 32767)
 		return sprintf(buf, "%u\n", qt_delta);
 	else
@@ -3195,7 +3237,9 @@ static ssize_t set_delta2_mode_show(struct device *dev, struct device_attribute 
 static ssize_t set_delta3_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_delta = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_DELTA_MODE, test_node[3], &qt_delta);
+	mutex_unlock(&lock);
 	if (qt_delta < 32767)
 		return sprintf(buf, "%u\n", qt_delta);
 	else
@@ -3207,7 +3251,9 @@ static ssize_t set_delta3_mode_show(struct device *dev, struct device_attribute 
 static ssize_t set_delta4_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	uint16_t qt_delta = 0;
+	mutex_lock(&lock);
 	read_dbg_data(QT_DELTA_MODE, test_node[4], &qt_delta);
+	mutex_unlock(&lock);
 	if (qt_delta < 32767)
 		return sprintf(buf, "%u\n", qt_delta);
 	else
@@ -3236,7 +3282,9 @@ static ssize_t set_all_refer_mode_show(struct device *dev, struct device_attribu
 {
 	int status = 0;
 
+	mutex_lock(&lock);
 	status = read_all_data(QT_REFERENCE_MODE);
+	mutex_unlock(&lock);
 
 	return sprintf(buf, "%u\n", status);
 }
@@ -3279,7 +3327,9 @@ static ssize_t set_all_delta_mode_show(struct device *dev, struct device_attribu
 {
 	int status = 0;
 
+	mutex_lock(&lock);
 	status = read_all_delta_data(QT_DELTA_MODE);
+	mutex_unlock(&lock);
 
 	return sprintf(buf, "%u\n", status);
 }
@@ -3334,6 +3384,7 @@ static ssize_t set_mxt_update_show(struct device *dev, struct device_attribute *
 			return -EINVAL;
 		}*/
 
+		mutex_lock(&lock);
 		disable_irq(data->client->irq);
 		firm_status_data = 1;
 		if (data->family_id == 0x80) {	/*  : MXT-224 */
@@ -3349,7 +3400,7 @@ static ssize_t set_mxt_update_show(struct device *dev, struct device_attribute *
 			dev_err(dev, "The firmware update failed(%d)\n", error);
 			firm_status_data = 3;
 			printk(KERN_ERR"[TSP The firmware update failed(%d)\n", error);
-			return error;
+			goto err;
 		} else {
 			dev_dbg(dev, "The firmware update succeeded\n");
 			firm_status_data = 2;
@@ -3367,18 +3418,23 @@ static ssize_t set_mxt_update_show(struct device *dev, struct device_attribute *
 		error = mxt224_backup(data);
 		if (error) {
 			printk(KERN_ERR "[TSP] mxt224_backup fail!!!\n");
-			return error;
+			goto err;
 		}
 
 	/* reset the touch IC. */
 	error = mxt224_reset(data);
 	if (error) {
 			printk(KERN_ERR"[TSP] mxt224_reset fail!!!\n");
-			return error;
+			goto err;
 	}
 
 	msleep(60);
+	mutex_unlock(&lock);
 	return count;
+
+err:
+	mutex_unlock(&lock);
+	return error;
 }
 
 static ssize_t set_mxt_firm_status_show(struct device *dev, struct device_attribute *attr, char *buf)
@@ -3421,8 +3477,10 @@ static ssize_t key_threshold_store(struct device *dev, struct device_attribute *
 		ret = get_object_info(copy_data, TOUCH_MULTITOUCHSCREEN_T9, &size_one, &address);
 		size_one = 1;
 		value = (u8)threshold;
+		mutex_lock(&lock);
 		write_mem(copy_data, address+(u16)object_register, size_one, &value);
 		read_mem(copy_data, address+(u16)object_register, (u8)size_one, &val);
+		mutex_unlock(&lock);
 
 		printk(KERN_ERR "[TSP] T%d Byte%d is %d\n", TOUCH_MULTITOUCHSCREEN_T9, object_register, val);
 	}
@@ -3470,6 +3528,7 @@ ssize_t set_tsp_for_inputmethod_store(struct device *dev, struct device_attribut
 		return 1;
 	}
 
+	mutex_lock(&lock);
 	if (*buf == '1' && (!is_inputmethod)) {
 		is_inputmethod = 1;
 		jump_limit = 5;
@@ -3550,6 +3609,7 @@ ssize_t set_tsp_for_inputmethod_store(struct device *dev, struct device_attribut
 	}
 
 	}
+	mutex_unlock(&lock);
 
 
 	return 1;
@@ -3558,7 +3618,9 @@ ssize_t set_tsp_for_inputmethod_store(struct device *dev, struct device_attribut
 static ssize_t mxt224_call_release_touch(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	printk(" %s is called\n", __func__);
+	mutex_lock(&lock);
 	TSP_forced_release_for_call();
+	mutex_unlock(&lock);
 	return sprintf(buf,"0\n");
 }
 static ssize_t mxt_touchtype_show(struct device *dev, struct device_attribute *attr, char *buf)
@@ -3627,7 +3689,7 @@ static DEVICE_ATTR(dbg_switch, S_IRUGO | S_IWUSR | S_IWGRP, NULL, mxt224_debug_s
 static struct attribute *qt602240_attrs[] = {
 	&dev_attr_object_show.attr,
 	&dev_attr_object_write.attr,
-	&dev_attr_dbg_switch,
+	&dev_attr_dbg_switch.attr,
 	NULL
 };
 
