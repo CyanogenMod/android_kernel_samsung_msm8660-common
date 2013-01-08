9619d926616100b6b97304b21ea7f7466f1aba8b
Ken Zhang <kenz@codeaurora.org>
2013-01-08 14:28:20 -0500

    msm: display: panel frame rate report
    Add metadata ioctl for retrieving panel frame rate.
    Get ready to remove reserved fields usage.
    
    Change-Id: I51a4aed9d85efd5f83a184100b6a3dc682c7a67c
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -75,6 +75,7 @@
 #define MSMFB_BUFFER_SYNC  _IOW(MSMFB_IOCTL_MAGIC, 162, struct mdp_buf_sync)
 #define MSMFB_DISPLAY_COMMIT      _IOW(MSMFB_IOCTL_MAGIC, 164, \
 						struct mdp_display_commit)
+#define MSMFB_METADATA_GET  _IOW(MSMFB_IOCTL_MAGIC, 166, struct msmfb_metadata)
 
 #define FB_TYPE_3D_PANEL 0x10101010
 #define MDP_IMGTYPE2_START 0x10000
@@ -538,6 +539,7 @@ struct msmfb_mdp_pp {
 enum {
 	metadata_op_none,
 	metadata_op_base_blend,
+	metadata_op_frame_rate,
 	metadata_op_max
 };
 
