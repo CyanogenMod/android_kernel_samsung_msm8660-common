a6ad7bfebef0d3bca3f246345332df527e6225f4
Kuogee Hsieh <khsieh@codeaurora.org>
2013-01-11 10:26:01 -0800

    msm_fb: display: add satge commit without pipes queued
    This patch allow satge commit without pipes queued to
    allow a pipe to be un-staged from mixer immediately after
    overlay_unset.
    
    Change-Id: I03abb907c66cce3d0559743d3bb1eb26a88dc78a
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_cmd.c
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_dtv.c
    	drivers/video/msm/mdp4_overlay_lcdc.c
    
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -168,10 +168,11 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait)
 	pipe = vctrl->base_pipe;
 	mixer = pipe->mixer_num;
 
-	if (vp->update_cnt == 0) {
-		mutex_unlock(&vctrl->update_lock);
-		return cnt;
-	}
+	/*
+	 * allow stage_commit without pipes queued
+	 * (vp->update_cnt == 0) to unstage pipes after
+	 * overlay_unset
+	 */
 
 	vctrl->update_ndx++;
 	vctrl->update_ndx &= 0x01;
@@ -1114,6 +1115,7 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 	unsigned int buf_offset;
 	int bpp;
 	int cndx = 0;
+	int cnt;
 	struct vsycn_ctrl *vctrl;
 	struct mdp4_overlay_pipe *pipe;
 
@@ -1146,10 +1148,13 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 	mdp4_overlay_mdp_perf_upd(mfd, 1);
 	mdp4_dsi_video_pipe_commit(0, 0);
 
-	if (pipe->ov_blt_addr)
-		mdp4_dsi_video_wait4ov(0);
-	else
-		mdp4_dsi_video_wait4dmap(0);
+	cnt = mdp4_dsi_video_pipe_commit(cndx, 0);
+	if (cnt >= 0) {
+		if (pipe->ov_blt_addr)
+			mdp4_dsi_video_wait4ov(cndx);
+		else
+			mdp4_dsi_video_wait4dmap(cndx);
+	}
 
 	mdp4_overlay_mdp_perf_upd(mfd, 0);
 	mutex_unlock(&mfd->dma->ov_mutex);
