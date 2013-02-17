e4831ecb37e0448c56940bc0a0a2af3a95e4e91f
Nick Reuter <nreuter85@gmail.com>
2013-02-17 17:15:24 -0600

    fix user-defined backlight scaling
    Change-Id: Id773c905beabe78d62e02b7c10cc61546d1969aa

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -249,5 +249,9 @@ int msm_fb_check_frame_rate(struct msm_fb_data_type *mfd,
 #ifdef CONFIG_FB_MSM_LOGO
 #define INIT_IMAGE_FILE "/initlogo.rle"
 int load_565rle_image(char *filename, bool bf_supported);
+#define LPM_INIT_IMAGE_FILE "/lpminitlogo.rle"
+#if (defined(CONFIG_TARGET_SERIES_P5LTE) || defined(CONFIG_TARGET_SERIES_P8LTE)) && defined(CONFIG_TARGET_LOCALE_KOR)
+#define CHARGING_IMAGE_FILE "/charging_image.rle"
+#endif
 #endif
 #endif /* MSM_FB_H */
