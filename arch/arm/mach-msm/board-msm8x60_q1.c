ba4530f379d057e4d359d0e9ec981ffed4957507
Nick Reuter <nreuter85@gmail.com>
2013-02-17 20:37:32 -0600

    fix warning: function is not a prototype
    Change-Id: I1d9d42cc0754efc3d565e5cd0d86f7eb960ecfe0

--- arch/arm/mach-msm/board-msm8x60_q1.c
+++ arch/arm/mach-msm/board-msm8x60_q1.c
@@ -1804,7 +1804,7 @@ fail:
 }
 
 #ifdef CONFIG_USB_HOST_NOTIFY
-static void msm_hsusb_set_autosw_pba()
+static void msm_hsusb_set_autosw_pba(void)
 {
 #ifdef CONFIG_USB_SWITCH_FSA9480
 	fsa9480_otg_set_autosw_pba();
