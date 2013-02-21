0fd179532bc7f593b4a79b4af9a89cdcf2ebf685
Steve Kondik <shade@chemlab.org>
2013-02-21 10:14:13 -0800

    video: msm: Better workaround for BLT+composition issue
     * Just check for this condition when the pipe is requested instead of
       causing an extra perf update.
    
    Change-Id: I9839b95d5726531671aa85bf5caa1035475bf53a

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2530,22 +2530,6 @@ static int mdp4_overlay_req2pipe(struct mdp_overlay *req, int mixer,
 
 	pipe->flags = req->flags;
 
-#ifdef CONFIG_ARCH_MSM8X60
-	/* If we're composing with MDP and we suddenly need a really outrageous clockrate,
-	 * just fail so that userspace switches to the GPU. If we don't do this, MDP will
-	 * switch to BLT mode, but it locks up and we never get any interrupts. Only seems to
-	 * happen on 8660 devices, so ifdeffed this appropriately. */
-	if (pipe->flags & MDP_BACKEND_COMPOSITION && req->id == MSMFB_NEW_REQUEST) {
-		mdp4_calc_pipe_mdp_clk(mfd, pipe);
-		if (pipe->req_clk > mdp_max_clk) {
-			pr_err("%s: high clock rate requested while composing, switch to GPU! req=%d max=%d",
-							__func__, pipe->req_clk, mdp_max_clk);
-			mdp4_overlay_pipe_free(pipe);
-			return -EINVAL;
-		}
-	}
-#endif
-
 	*ppipe = pipe;
 
 	return 0;
@@ -2832,6 +2816,13 @@ int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd,
 			worst_mdp_bw = pipe->req_bw;
 
 		if (mfd->mdp_rev == MDP_REV_41) {
+
+			if ((pipe->flags & MDP_BACKEND_COMPOSITION) && perf_req->use_ov0_blt) {
+				pr_err("%s: BLT requested while composing, switch to GPU! req=%d max=%d",
+						__func__, pipe->req_clk, mdp_max_clk);
+				return -EINVAL;
+			}
+
 			/*
 			 * writeback (blt) mode to provide work around
 			 * for dsi cmd mode interface hardware bug.
@@ -2878,6 +2869,7 @@ int mdp4_overlay_mdp_pipe_req(struct mdp4_overlay_pipe *pipe,
 		       __func__, ret);
 		ret = -EINVAL;
 	}
+
 	if (mdp4_calc_pipe_mdp_bw(mfd, pipe)) {
 		pr_err("%s unable to calc mdp pipe bandwidth ret=%d\n",
 		       __func__, ret);
@@ -3194,11 +3186,11 @@ int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 	if (hdmi_prim_display)
 		fill_black_screen(FALSE, pipe->pipe_num, pipe->mixer_num);
 
-	mdp4_overlay_mdp_pipe_req(pipe, mfd);
+	ret = mdp4_overlay_mdp_pipe_req(pipe, mfd);
 
 	mutex_unlock(&mfd->dma->ov_mutex);
 
-	return 0;
+	return ret;
 }
 
 int mdp4_overlay_unset_mixer(int mixer)
