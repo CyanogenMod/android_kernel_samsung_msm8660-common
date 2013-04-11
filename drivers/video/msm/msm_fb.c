70b847036b41f77440ae10cddba632ea667d2418
Steve Kondik <shade@chemlab.org>
2013-04-11 07:47:21 -0700

    video: msm: Clean up ioctls
     * Match ioctl numbers with userspace, remove unused code.
    
    Change-Id: I3493d1e2ecf2717a6782157311db413af2779250

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -3722,11 +3722,6 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = msmfb_overlay_unset(info, argp);
 		break;
 
-	case MSMFB_OVERLAY_COMMIT:
-		down(&msm_fb_ioctl_ppp_sem);
-		ret = msmfb_overlay_commit(info);
-		up(&msm_fb_ioctl_ppp_sem);
-		break;
 	case MSMFB_OVERLAY_PLAY:
 		ret = msmfb_overlay_play(info, argp);
 		break;
