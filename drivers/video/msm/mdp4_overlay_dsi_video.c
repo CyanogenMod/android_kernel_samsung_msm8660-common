0d5a37ac013c9560876397d420d0a0748b648c1a
Steve Kondik <shade@chemlab.org>
2013-05-08 15:59:23 -0700

    Revert "msm_fb: display: free base layer pipe at adb stop"
    This reverts commit d6825fb630a6d576b64c58690844d94aef5aecb7.
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_lcdc.c
    
    Change-Id: Iaf77b0672b8b816a4ea34e687420f30ec203ba0d

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -902,7 +902,6 @@ int mdp4_dsi_video_off(struct platform_device *pdev)
 			/* adb stop */
 			if (pipe->pipe_type == OVERLAY_TYPE_BF)
 				mdp4_overlay_borderfill_stage_down(pipe);
-
 			/* base pipe may change after borderfill_stage_down */
 			pipe = vctrl->base_pipe;
 			mdp4_mixer_stage_down(pipe, 1);
