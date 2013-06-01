c6316b63525cf5cad8172e1c937c3366c84457f7
carvsdriver <carvsdriver@gmail.com>
2013-06-01 12:09:25 -0400

    cpufreq: enforce governor to stay in sync across CPU cores
    This is a shameless port of work done by @elbermu along
    with some small changes to clean up the code a bit.
    
    This patch forces both CPU0 & CPU1 to stay on the same
    policy governor.  Currently, CPU1 defaults to a seemingly
    random governor (usually intellidemand) regardless of what
    governor the user selects.
    
    Change-Id: I448e452f2ce572309c5b9b405ae6f18d3de3d3a0

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -475,6 +475,8 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	char *envp[3];
 	char buf1[64];
 	char buf2[64];
+	int alt_cpu;
+	struct cpufreq_policy* alt_policy;
 
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);
 	if (ret)
@@ -494,6 +496,22 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 
 	policy->user_policy.policy = policy->policy;
 	policy->user_policy.governor = policy->governor;
+
+	alt_cpu = policy->cpu ? 0 : 1;
+	if(!cpu_online(alt_cpu))
+		cpu_up(alt_cpu);
+
+	if(!cpufreq_get_policy(&new_policy, alt_cpu)) {
+		alt_policy=cpufreq_cpu_get(alt_cpu);
+
+		if(alt_policy != NULL) {
+			cpufreq_parse_governor(str_governor, &new_policy.policy, &new_policy.governor);
+			__cpufreq_set_policy(alt_policy, &new_policy);
+			alt_policy->user_policy.policy = alt_policy->policy;
+			alt_policy->user_policy.governor = alt_policy->governor;
+			cpufreq_cpu_put(alt_policy);
+		}
+	}
 
 	sysfs_notify(&policy->kobj, NULL, "scaling_governor");
 
