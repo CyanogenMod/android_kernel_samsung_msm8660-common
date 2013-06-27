efee8d734af170f2d35e99106758d96333de0a73
Maheshwar Ajja <majja@codeaurora.org>
2013-06-27 20:10:23 +0530

    msm: vidc: add support for the bitstream_restrict flag
    Adds support for the MFC core to add the bitstream_restrict flag in the
    sps/pps for H264 codecs.  This allows for the decoder to determine how
    many frames to buffer when decoding.
    
    Change-Id: I0415de0704d0f0d5099cc97ca7937c955e504883
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_shared_mem.c
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_shared_mem.c
@@ -83,6 +83,8 @@
 #define VIDC_SM_ENC_EXT_CTRL_ADDR                    0x0028
 #define VIDC_SM_ENC_EXT_CTRL_VBV_BUFFER_SIZE_BMSK    0xffff0000
 #define VIDC_SM_ENC_EXT_CTRL_VBV_BUFFER_SIZE_SHFT    16
+#define VIDC_SM_ENC_EXT_CTRL_STREAM_RESTRICT_EN_BMSK 0x2000
+#define VIDC_SM_ENC_EXT_CTRL_STREAM_RESTRICT_EN_SHFT 13
 #define VIDC_SM_ENC_EXT_CTRL_H263_CPCFC_ENABLE_BMSK  0x80
 #define VIDC_SM_ENC_EXT_CTRL_H263_CPCFC_ENABLE_SHFT  7
 #define VIDC_SM_ENC_EXT_CTRL_SPS_PPS_CONTROL_BMSK    0X100
@@ -396,7 +398,8 @@ void vidc_sm_set_extended_encoder_control(struct ddl_buf_addr
 	*shared_mem, u32 hec_enable,
 	enum VIDC_SM_frame_skip frame_skip_mode,
 	u32 seq_hdr_in_band, u32 vbv_buffer_size, u32 cpcfc_enable,
-	u32 sps_pps_control, u32 closed_gop_enable)
+	u32 sps_pps_control, u32 closed_gop_enable,
+	u32 restrict_bitstream_enable)
 {
 	u32 enc_ctrl;
 
@@ -420,7 +423,11 @@ void vidc_sm_set_extended_encoder_control(struct ddl_buf_addr
 			VIDC_SM_ENC_EXT_CTRL_SPS_PPS_CONTROL_BMSK) |
 			VIDC_SETFIELD(closed_gop_enable,
 			VIDC_SM_ENC_EXT_CTRL_CLOSED_GOP_ENABLE_SHFT,
-			VIDC_SM_ENC_EXT_CTRL_CLOSED_GOP_ENABLE_BMSK);
+			VIDC_SM_ENC_EXT_CTRL_CLOSED_GOP_ENABLE_BMSK) |
+			VIDC_SETFIELD((restrict_bitstream_enable) ? 1 : 0,
+			VIDC_SM_ENC_EXT_CTRL_STREAM_RESTRICT_EN_SHFT,
+			VIDC_SM_ENC_EXT_CTRL_STREAM_RESTRICT_EN_BMSK);
+
 	DDL_MEM_WRITE_32(shared_mem, VIDC_SM_ENC_EXT_CTRL_ADDR, enc_ctrl);
 }
 
