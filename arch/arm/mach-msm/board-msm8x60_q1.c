717bb100cd50eaa2c8316fcdcb0b6c0f1c1de959
Mike Grissom <mikeyman77@gmail.com>
2013-07-28 18:54:56 -0700

    board-msm8x60: Q1: fix a ifdef derp
    Change-Id: I4aa56887f322ca86cf4528ce8dc37a4cb0a4dd93

--- arch/arm/mach-msm/board-msm8x60_q1.c
+++ arch/arm/mach-msm/board-msm8x60_q1.c
@@ -3587,6 +3587,10 @@ static struct platform_device msm_gemini_device = {
 #endif
 
 #ifdef CONFIG_I2C_QUP
+static void gsbi_qup_i2c_gpio_config(int adap_id, int config_type)
+{
+}
+
 #if defined(CONFIG_JPN_MODEL_SC_05D)
 /*	QC patch for case 00580204 , I2C QTR failure
 * GSBI7 GPIO configuration for recovery of QTR I2C lines
@@ -3604,10 +3608,6 @@ static uint32_t gsbi7_i2c_table[] = {
 	GPIO_CFG(GSBI7_SDA, 1, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_8MA),
 	GPIO_CFG(GSBI7_SCL, 1, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_8MA),
 };
-
-static void gsbi_qup_i2c_gpio_config(int adap_id, int config_type)
-{
-}
 
 //QC patch for case 00580204 , I2C QTR failure	//rohbt
 static void gsbi7_qup_i2c_gpio_config(int adap_id, int config_type)
